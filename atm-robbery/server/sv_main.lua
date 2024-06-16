952c7deb8befaf0fb67afe3b051642b3e2cdf0f9
OffSey <87211817+OffSey@users.noreply.github.com>
2024-06-16 15:08:27 +0200

    Update sv_main.lua
    Add Logs

--- atm-robbery/server/sv_main.lua
+++ atm-robbery/server/sv_main.lua
@@ -4,8 +4,14 @@ if Config.Framework then
         ESX = exports['es_extended']:getSharedObject()
     elseif Config.Framework == "Qb" then
         local QBCore = exports['qb-core']:GetCoreObject()
-        end
     end
+end
+
+local function formatMessage(template, variables)
+    return (template:gsub("{{(.-)}}", function(key)
+        return tostring(variables[key] or key)
+    end))
+end
 
 local lastRobbedTime = 0
 local cooldownTime = 900
@@ -14,38 +20,48 @@ local reward = Config.GainStolen
 RegisterServerEvent("OffSeyATM:AttemptRob")
 AddEventHandler("OffSeyATM:AttemptRob", function()
     local currentTime = os.time()
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local playerName = xPlayer.getName()
+    local playerId = source
+    local playerIdentifier = xPlayer.identifier
 
     if currentTime - lastRobbedTime >= cooldownTime then
         lastRobbedTime = currentTime
-
         TriggerClientEvent("OffSey:AtmRob", source)
 
         if Config.RemoveItem then
-            if Config.RemoveItem == "true" then
-                framework.removeItem({ player = source, item = Config.ItemRequire, count = 1 })
-            elseif Config.RemoveItem == "false" then
-                print('no remove item')
-            end
+            framework.removeItem({ player = source, item = Config.ItemRequire, count = 1 })
+        else
+            print('no remove item')
+        end
+
+        webhooks(formatMessage(locale('logs_success'), {playerName = playerName, playerId = playerId, playerIdentifier = playerIdentifier}), 3066993)
     else
         local timeLeft = math.ceil((cooldownTime - (currentTime - lastRobbedTime)) / 60)
-        TriggerClientEvent('OffSey:showNotification', source, locale('notification_title'), locale('notification_timeleft_partone') ..timeLeft.. locale('notification_timeleft_parttwo'), 'error')   
-        end
+        TriggerClientEvent('OffSey:showNotification', source, locale('notification_title'), locale('notification_timeleft_partone') .. timeLeft .. locale('notification_timeleft_parttwo'), 'error')
+        
+        webhooks(formatMessage(locale('logs_failure'), {playerName = playerName, playerId = playerId, playerIdentifier = playerIdentifier, timeLeft = timeLeft}), 15158332)
     end
 end)
 
-
-
 RegisterServerEvent("OffSeyATM:Recompense")
 AddEventHandler("OffSeyATM:Recompense", function()
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local playerName = xPlayer.getName()
+    local playerId = source
+    local playerIdentifier = xPlayer.identifier
+
     if Config.Framework then
         if Config.Framework == "ESX" then
             framework.addMoneyATM({ player = source, amount = reward })
-            TriggerClientEvent('OffSey:showNotification', source, locale('notification_title'), locale('notification_stolen_gain') ..reward, 'info')  
+            TriggerClientEvent('OffSey:showNotification', source, locale('notification_title'), locale('notification_stolen_gain') .. reward, 'info')
         elseif Config.Framework == "Qb" then
             framework.addMoneyATM({ player = source, amount = reward })
-            TriggerClientEvent('OffSey:showNotification', source, locale('notification_title'), locale('notification_stolen_gain') ..reward, 'info')  
+            TriggerClientEvent('OffSey:showNotification', source, locale('notification_title'), locale('notification_stolen_gain') .. reward, 'info')
         end
     end
+
+    webhooks(formatMessage(locale('logs_gain'), {playerName = playerName, playerId = playerId, playerIdentifier = playerIdentifier, reward = reward}), 3066993)
 end)
 
 RegisterServerEvent('OffSey:Server:PoliceAlert')
@@ -53,28 +69,27 @@ AddEventHandler('OffSey:Server:PoliceAlert', function(coords)
     if Config.Framework then
         if Config.Framework == "ESX" then
             local players = ESX.GetPlayers()
-                for i = 1, #players do
-                    local player = ESX.GetPlayerFromId(players[i])
-                        for k, v in pairs(Config.PoliceJob) do
-                            if player.job.name == v then
-                                TriggerClientEvent('OffSey:Client:PoliceAlert', players[i], coords)
+            for i = 1, #players do
+                local player = ESX.GetPlayerFromId(players[i])
+                for k, v in pairs(Config.PoliceJob) do
+                    if player.job.name == v then
+                        TriggerClientEvent('OffSey:Client:PoliceAlert', players[i], coords)
+                    end
+                end
+            end
         elseif Config.Framework == "Qb" then
             local players = QBCore.Functions.GetPlayers()
-                for i = 1, #players do
-                    local player =  QBCore.Functions.GetPlayer(players[i])
-                        for k, v in pairs(Config.PoliceJob) do
-                            if player.job.name == v then
-                                TriggerClientEvent('OffSey:Client:PoliceAlert', players[i], coords)
-                                end
-                            end
-                        end
+            for i = 1, #players do
+                local player = QBCore.Functions.GetPlayer(players[i])
+                for k, v in pairs(Config.PoliceJob) do
+                    if player.job.name == v then
+                        TriggerClientEvent('OffSey:Client:PoliceAlert', players[i], coords)
                     end
                 end
             end
         end
     end
 end)
-
 
 lib.callback.register('OffSeyATM:HasItem', function(source)
     return framework.hasItems({ player = source, items = Config.ItemRequire })
