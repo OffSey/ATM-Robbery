606771fc6d64a72a18d99acdf74de5f75f9d115d
OffSey <87211817+OffSey@users.noreply.github.com>
2024-06-16 15:07:34 +0200

    Update config.lua
    Add webhookslinks

--- atm-robbery/config.lua
+++ atm-robbery/config.lua
@@ -4,17 +4,18 @@ Config.Framework = "ESX" -- ESX or Qb
 Config.AccountGainESX = "black_money" -- black_money/money/bank // For ESX
 Config.AccountGainQB = "cash" -- cash/bank // For QbCore
 
-
 -- Target
 Config.Target = "ox_target" -- ox_target or qb-target
 
 -- Dispatch & Police
-Config.Dispatch = "default" -- cd_dispatch | qs-dispatch | rcore_dispatch | ps-dispatch| default
+Config.Dispatch = "default" -- cd_dispatch | qs-dispatch | rcore_dispatch | ps-dispatch | default
 Config.PoliceJob = {'police', 'sheriff'}
 
 -- Item
 Config.ItemRequire = "hacking_computer"
-Config.RemoveItem = "true" -- true or false
+Config.RemoveItem = true -- true or false
 
 -- Gain Stolen
 Config.GainStolen = math.random(10000, 15000)
+
+Config.WebhooksLinks = "WebhooksLinks"
